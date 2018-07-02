8b37924d6984be64d5204bffa382302de9cd0034
gpernas <gpernas@DCT00116.madrid.dacartec.com>
2018-07-02 10:53:46 +0200

    Commit numero 1

---
+++ src/com/gonzalo/Imprime.java
@@ -0,0 +1,10 @@
+package com.gonzalo;
+
+public class Imprime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola");
+	}
+
+}
---
+++ src/com/gonzalo/Principal.java
@@ -0,0 +1,9 @@
+package com.gonzalo;
+
+public class Principal {
+
+		public static void main()
+		{
+			System.out.println("Hola");
+		}
+}
